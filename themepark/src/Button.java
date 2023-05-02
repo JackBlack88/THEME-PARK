978b9082a9b9e4405faebe9eccecbccdddfc9849
JackBlack88 <130683977+JackBlack88@users.noreply.github.com>
2023-05-02 14:31:54 -0400

    still working on the button constructor

--- themepark/src/Button.java
+++ themepark/src/Button.java
@@ -3,9 +3,8 @@ public class Button {
    private int height;
    private int width;
    private String color;
-
-
-
+   
+   
 public Button (String color, int height, int width){
 setWidth(width);
 setColor(color);
@@ -14,6 +13,7 @@ setHeight(height);
 }
 //encaptlation
    public String getColor(){
+      
     return color;  
    }
    public int getHeight(){
@@ -21,11 +21,19 @@ setHeight(height);
       return height;  
      }
      public int getWidth(){
-      width = 100;
+      
       return width;  
      }
      public boolean getClicked(){
       return clicked;  
      }
-   
+     public void setColor(String color){
+this.color = color;
+     }
+   public void setWidth(int color){
+      this.width = width;
+     }
+     public void setHeight(int color){
+      this.height = height;
+     }
 }
