f362fb9c6d0355a5e298aeecc92e7c2ab92178dd
Liam Porter <130683977+JackBlack88@users.noreply.github.com>
2023-05-03 17:03:00 -0400

    Cleaned up code and put my JFrame, JPanel, and JButton in my app class

--- themepark/src/App.java
+++ themepark/src/App.java
@@ -5,23 +5,28 @@ import java.awt.Color;
 import java.awt.GridLayout;
 
 public class App {
+    private JFrame frame;
+    private JButton button;
+    private JPanel panel;
 
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame();
+    public App() {
+        frame = new JFrame();
         frame.setBackground(Color.white);
         frame.setSize(420, 420);
         frame.setTitle("COASTER GAME");
         frame.setVisible(true);
         // frame.setIconImage(image.getImage());
         // frame.setIconImage(image.getImage());
-        JButton button = new JButton();
+        button = new JButton();
         button.setVisible(true);
 
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setBorder(null);
 
         panel.add(button);
+    }
 
+    public static void main(String[] args) {
+        new App();
     }
 }
--- themepark/src/Window.java
+++ themepark/src/Window.java
@@ -1,5 +1,2 @@
 
 
-public class Window {
-    
-}
