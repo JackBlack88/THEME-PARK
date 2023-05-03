1769bfc3c8a3b0ae71d4f12917e03dbe641be743
JackBlack88 <130683977+JackBlack88@users.noreply.github.com>
2023-05-03 13:43:19 -0400

    Cleaning up the app and making a panel

--- themepark/src/App.java
+++ themepark/src/App.java
@@ -1,19 +1,35 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.GridLayout;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    private JFrame frame;
+    private JButton button;
+    private JPanel panel;
 
-        
-        JFrame frame = new JFrame();
+    public static void main(String[] args) {
+
+    }
+
+    public void button() {
+        button = new JButton();
+        button.setVisible(true);
+    }
+
+    public void frame() {
+        frame = new JFrame();
         frame.setBackground(Color.white);
         frame.setSize(420, 420);
         frame.setTitle("COASTER GAME");
         frame.setVisible(true);
-        JButton button = new JButton();
-        button.setVisible(true);
-        //frame.setIconImage(image.getImage());
-        //frame.setIconImage(image.getImage());
+        // frame.setIconImage(image.getImage());
+        // frame.setIconImage(image.getImage());
+    }
+
+    public void panel() {
+        panel = new JPanel();
+        panel.setBorder(null);
     }
 }
--- themepark/src/Button.java
+++ themepark/src/Button.java
@@ -3,37 +3,43 @@ public class Button {
    private int height;
    private int width;
    private String color;
-   
-   
-public Button (String color, int height, int width){
-setWidth(width);
-setColor(color);
-setHeight(height);
 
-}
-//encaptlation
-   public String getColor(){
-      
-    return color;  
+   public Button(String color, int height, int width) {
+      setWidth(width);
+      setColor(color);
+      setHeight(height);
+
    }
-   public int getHeight(){
-      
-      return height;  
-     }
-     public int getWidth(){
-      
-      return width;  
-     }
-     public boolean getClicked(){
-      return clicked;  
-     }
-     public void setColor(String color){
-this.color = color;
-     }
-   public void setWidth(int color){
+
+   // encaptlation
+   public String getColor() {
+
+      return color;
+   }
+
+   public int getHeight() {
+
+      return height;
+   }
+
+   public int getWidth() {
+
+      return width;
+   }
+
+   public boolean getClicked() {
+      return clicked;
+   }
+
+   public void setColor(String color) {
+      this.color = color;
+   }
+
+   public void setWidth(int color) {
       this.width = width;
-     }
-     public void setHeight(int color){
+   }
+
+   public void setHeight(int color) {
       this.height = height;
-     }
+   }
 }
