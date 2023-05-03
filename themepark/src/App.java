e21a0c2d250b93cdd9f9d3f1bc80d3b1e71b42f5
Liam Porter <130683977+JackBlack88@users.noreply.github.com>
2023-05-03 17:22:54 -0400

    Added the panel to the frame and made an action listener

--- themepark/src/App.java
+++ themepark/src/App.java
@@ -1,32 +1,44 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class App {
+public class App implements ActionListener {
     private JFrame frame;
     private JButton button;
     private JPanel panel;
 
     public App() {
+
         frame = new JFrame();
-        frame.setBackground(Color.white);
+        //frame.setBackground(Color.white);
         frame.setSize(420, 420);
         frame.setTitle("COASTER GAME");
         frame.setVisible(true);
         // frame.setIconImage(image.getImage());
         // frame.setIconImage(image.getImage());
-        button = new JButton();
-        button.setVisible(true);
+        button = new JButton("Button");
+        
 
         panel = new JPanel();
-        panel.setBorder(null);
+
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 50, 100, 100));
 
         panel.add(button);
+        frame.add(panel);
     }
 
     public static void main(String[] args) {
         new App();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
     }
 }
