d31e48ae6b01cbfb7948606b2bcd1eb4dea6b92a
JackBlack88 <130683977+JackBlack88@users.noreply.github.com>
2023-05-03 14:04:16 -0400

    added panel and added button to the panel

--- themepark/src/App.java
+++ themepark/src/App.java
@@ -5,31 +5,23 @@ import java.awt.Color;
 import java.awt.GridLayout;
 
 public class App {
-    private JFrame frame;
-    private JButton button;
-    private JPanel panel;
 
     public static void main(String[] args) {
 
-    }
-
-    public void button() {
-        button = new JButton();
-        button.setVisible(true);
-    }
-
-    public void frame() {
-        frame = new JFrame();
+        JFrame frame = new JFrame();
         frame.setBackground(Color.white);
         frame.setSize(420, 420);
         frame.setTitle("COASTER GAME");
         frame.setVisible(true);
         // frame.setIconImage(image.getImage());
         // frame.setIconImage(image.getImage());
-    }
+        JButton button = new JButton();
+        button.setVisible(true);
 
-    public void panel() {
-        panel = new JPanel();
+        JPanel panel = new JPanel();
         panel.setBorder(null);
+
+        panel.add(button);
+
     }
 }
