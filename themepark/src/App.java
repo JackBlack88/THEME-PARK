fdfa7cc1f98276d44b56dc2ba89da41230554574
JackBlack88 <130683977+JackBlack88@users.noreply.github.com>
2023-05-05 14:12:20 -0400

    made it so my button will be removed and re added

--- themepark/src/App.java
+++ themepark/src/App.java
@@ -30,21 +30,38 @@ public class App implements ActionListener {
         panel = new JPanel();
 
         panel.setBorder(BorderFactory.createEmptyBorder(100, 50, 100, 100));
+        panel.setLayout(null);
 
         panel.add(button);
+        button.setSize(200, 100);
+        button.setLocation(30, 30);
+
         frame.add(panel);
+
         label = new JLabel("NUMBER OF CLICKS: 0");
+        label.setSize(230, 70);
         panel.add(label);
+
+        label.setLocation(300, 0);
 
     }
 
     public static void main(String[] args) {
         new App();
+
+    }
+
+    public void button() {
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         count++;
         label.setText("NUMBER OF CLICKS:" + count);
+        panel.remove(button);
+        this.frame.repaint();
+        panel.add(button);
+
     }
 }
