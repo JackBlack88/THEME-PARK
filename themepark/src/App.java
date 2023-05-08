b1e2065668cffa5c80879ec1cd52f2acabf4d6cc
Liam Porter <130683977+JackBlack88@users.noreply.github.com>
2023-05-08 18:46:24 -0400

    Finished cleaning up my code

--- themepark/src/App.java
+++ themepark/src/App.java
@@ -1,98 +1,85 @@
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import java.awt.Transparency;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.lang.annotation.Target;
-import javax.swing.Timer;
+import javax.swing.*;
+import java.awt.event.*;
+
 public class App implements ActionListener {
-    private JFrame main;
+    private JFrame mainFrame;
     private JFrame gameFrame;
-    private JFrame end;
     private JButton button;
     private JButton startButton;
     private JPanel panel;
-    private JPanel first;
-    private JPanel ending;
-    private JLabel score;
+    private JPanel startPanel;
+    private JLabel scoreLabel;
     private JLabel label;
-    int count;
-    Frame f = new Frame(1000, 1000);
-    Button target = new Button(189, 100, 0, 600);
-    ImageIcon ballon = new ImageIcon("ballon.gif");
+    private int count;
+    private Frame f = new Frame(1000, 1000);
+    private Button target = new Button(189, 100, 0, 600);
+    private ImageIcon balloonIcon = new ImageIcon("balloon.gif");
 
     public App() {
-        main = new JFrame();
-        main.setVisible(true);
-        main.setSize(f.getWidth(), f.getHeight());
-        first = new JPanel();
+        mainFrame = new JFrame();
+        mainFrame.setSize(f.getWidth(), f.getHeight());
 
+        startPanel = new JPanel();
         startButton = new JButton("PRESS TO START");
         startButton.setSize(target.getWidth(), target.getHeight());
-        main.add(first);
-        first.add(startButton);
-
         startButton.addActionListener(this);
+        startPanel.add(startButton);
 
+        mainFrame.add(startPanel);
+        mainFrame.setVisible(true);
     }
 
     public static void main(String[] args) {
         new App();
     }
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        Object o = e.getSource();
-        if (o == button) {
+        Object source = e.getSource();
+
+        if (source == button) {
             count++;
-            label.setText("NUMBER OF CLICKS:" + count);
+            scoreLabel.setText("NUMBER OF CLICKS: " + count);
+
             panel.remove(button);
-            this.gameFrame.repaint();
             panel.add(button);
+
             int min = 0;
             int max = 500;
-            double x = min + Math.random() * (max - min); // getting random x-axis and y-axis vaules
+            double x = min + Math.random() * (max - min); // getting random x-axis and y-axis values
             double y = min + Math.random() * (max - min);
             int xValue = (int) x; // Converting the x and y nums from doubles to ints so I can set the location
             int yValue = (int) y;
             button.setLocation(xValue, yValue);
-        }
 
-        else if (o == startButton) {
+            gameFrame.repaint();
+        } else if (source == startButton) {
             gameFrame = new JFrame();
-            button = new JButton(ballon);// Makes a new button and makes the button into the "ballon" gif
-            panel = new JPanel();
-            label = new JLabel("NUMBER OF CLICKS: 0");
-            gameFrame.setVisible(true);
             gameFrame.setSize(f.getWidth(), f.getHeight());
-            gameFrame.setTitle("BALLON POPPING");
+            gameFrame.setTitle("BALLOON POPPING");
             gameFrame.setVisible(true);
 
+            button = new JButton(balloonIcon);
             button.addActionListener(this);
             button.setOpaque(false);
             button.setBorderPainted(false);
             button.setContentAreaFilled(false);
+            button.setSize(target.getWidth(), target.getHeight());
+            button.setLocation(30, 30);
+
+            scoreLabel = new JLabel("NUMBER OF CLICKS: 0");
+            scoreLabel.setSize(230, 70);
+            scoreLabel.setLocation(300, 0);
+
+            panel = new JPanel();
             panel.setSize(600, 600);
             panel.setBorder(BorderFactory.createEmptyBorder(100, 50, 100, 100));
             panel.setLayout(null);
             panel.add(button);
+            panel.add(scoreLabel);
 
-            button.setSize(target.getWidth(), target.getHeight());
-            button.setLocation(30, 30);
- 
-            label.setSize(230, 70);
-            panel.add(label);
-
-            label.setLocation(300, 0);
-
-            main.dispose();
+            gameFrame.add(panel);
+            mainFrame.dispose();
         }
-        
     }
 }
